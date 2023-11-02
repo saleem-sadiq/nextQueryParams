bed7a6e5e654120732eb470c125a81558dd2cab3
Nabeel-Sadiq <sadiqnabeel4@gmail.com>
2023-11-02 15:00:03 +0500

    search query params code done for product pages

--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image"
+import Link from "next/link";
 
-const imageUrls = {
+const imageUrls: any = {
     black: "https://demo.vercel.store/_next/image?url=https%3A%2F%2Fcdn.shopify.com%2Fs%2Ffiles%2F1%2F0754%2F3727%2F7491%2Ffiles%2Ft-shirt-1.png%3Fv%3D1689798965&w=1200&q=75",
     white: "https://demo.vercel.store/_next/image?url=https%3A%2F%2Fcdn.shopify.com%2Fs%2Ffiles%2F1%2F0754%2F3727%2F7491%2Ffiles%2Ft-shirt-2.png%3Fv%3D1689798965&w=1200&q=75",
     blue: "https://demo.vercel.store/_next/image?url=https%3A%2F%2Fcdn.shopify.com%2Fs%2Ffiles%2F1%2F0754%2F3727%2F7491%2Ffiles%2Ft-shirt-circles-blue.png%3Fv%3D1690003396&w=1200&q=75",
@@ -9,9 +10,12 @@ const imageUrls = {
 const colorVariants = ["black", "white", "blue"];
 const sizeVariants = ["xs", "s", "md", "lg", "xl"];
 
-const page = () => {
-    const selectedColor = "black";
-    const selectedSize = "md";
+const page = ({ searchParams }: {
+    searchParams: { [key: string]: string | string[] | undefined }
+}) => {
+    const selectedColor = (searchParams.color || "black") as string;
+    const selectedSize = (searchParams.size || "md") as string;
+
     return (
         <div className="flex justify-center items-center w-full min-h-screen bg-slate-300">
             <div className="w-[70rem] h-[30rem] bg-white flex">
@@ -27,13 +31,21 @@ const page = () => {
                 <div className="w-[50%] h-full ">
                     <h1 className="text-3xl font-bold text-left mt-20">Acme Circle T-Shirt</h1>
                     <h2 className="p-2 rounded-3xl bg-blue-500 w-28 text-center mt-5">$200 USD</h2>
-                    <h2 className="mt-5 text-xl font-semibold">Color</h2>
+                    <h2 className="my-5 text-xl font-semibold">Color</h2>
                     {colorVariants.map((color, index) => (
-                        <button key={index} className="p-2 rounded-3xl bg-slate-200 w-28 text-center mt-5 mx-2">{color}</button>
+                        <Link 
+                        // We use this type of href because if we have special charachter in our query params then we go for this
+                        href={`?${new URLSearchParams({
+                            color,
+                            size: selectedSize,
+                        })}`} key={index} className={`px-5 py-2 rounded-3xl bg-slate-200 w-28 text-center mt-5 mx-2 border-2 ${selectedColor === color ? "border-blue-500" : "border-white"}`}>{color}</Link>
                     ))}
-                    <h2 className="mt-5 text-xl font-semibold">Size</h2>
+                    <h2 className="my-5 text-xl font-semibold">Size</h2>
                     {sizeVariants.map((size, index) => (
-                        <button key={index} className="p-2 rounded-3xl bg-slate-200 w-28 text-center mt-5 mx-2">{size}</button>
+                        <Link href={`?${new URLSearchParams({
+                            color: selectedColor,
+                            size,
+                        })}`} key={index} className={`px-5 py-2 rounded-3xl bg-slate-200 w-28 text-center mt-5 mx-2 border-2 ${selectedSize === size ? "border-blue-500" : "border-white"}`}>{size}</Link>
                     ))}
                 </div>
             </div>
